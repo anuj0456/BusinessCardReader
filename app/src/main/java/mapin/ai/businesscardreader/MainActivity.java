bb7a7c97b15d9ecac54051a4596bc175322515b6
anuj0456 <anuj0456@gmail.com>
2023-01-12 20:56:52 +0530

    Initial commit

---
+++ app/src/main/java/mapin/ai/businesscardreader/MainActivity.java
@@ -0,0 +1,44 @@
+package mapin.ai.businesscardreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+
+import mapin.ai.businesscardreader.utility.Assets;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+    private MainViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        text = findViewById(R.id.text);
+        Assets.extractAssets(this);
+
+        if (!viewModel.isInitialized()) {
+            String dataPath = Assets.getTessDataPath(this);
+            String language = Assets.getLanguage();
+            viewModel.initTesseract(dataPath, language, TessBaseAPI.OEM_LSTM_ONLY);
+        }
+
+        File imageFile = Assets.getImageFile(this);
+        viewModel.recognizeImage(imageFile);
+
+        viewModel.getResult().observe(this, result -> {
+            text.setText(result);
+        });
+
+
+
+    }
+}
